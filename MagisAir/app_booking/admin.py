37a0b2c45384eb7549ed970956bfdc91ba181b36
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2023-11-12 17:08:02 +0800

    Added Ticket Functionality to app_booking

--- MagisAir/app_booking/admin.py
+++ MagisAir/app_booking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Passenger, Item, Booking, BookingItem
+from .models import Passenger, Item, Booking, BookingItem, Ticket
 
 # Register your models here.
 @admin.register(Passenger)
@@ -24,3 +24,10 @@ class BookingItemAdmin(admin.ModelAdmin):
     list_filter = ('booking_id', 'item_id')
     search_fields = ('booking_id__booking_id', 'item_id__item_name')  # assuming Booking and Item models have these attributes
     readonly_fields = ('booking_item_cost',)  # Make booking_item_cost read-only in the admin panel
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('ticket_id', 'booking_id', 'scheduled_flight_id', 'seat_class', 'seat_number', 'ticket_cost')
+    list_filter = ('booking_id', 'scheduled_flight_id')
+    search_fields = ('booking_id__booking_id', 'scheduled_flight_id__scheduled_flight_id')
+    readonly_fields = ('ticket_cost',)
--- MagisAir/app_booking/models.py
+++ MagisAir/app_booking/models.py
@@ -1,4 +1,7 @@
+import re
+from django.core.exceptions import ValidationError
 from django.db import models
+from app_schedule.models import ScheduledFlight
 
 # Create your models here.
 class Passenger(models.Model):
@@ -51,3 +54,35 @@ class BookingItem(models.Model):
 
     def __str__(self):
         return f"{self.item_id} | {self.booking_id}"
+
+class Ticket(models.Model):
+    ticket_id = models.AutoField(primary_key=True)
+    booking_id = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    scheduled_flight_id = models.ForeignKey(ScheduledFlight, on_delete=models.CASCADE)
+    SEAT_CLASS_CHOICES = [
+        ('1st Class', '1st Class'),
+        ('Business Class', 'Business Class'),
+        ('Premium Economy Class', 'Premium Economy Class'),
+        ('Regular Economy Class', 'Regular Economy Class')
+    ]
+    seat_class = models.CharField(max_length=100, choices=SEAT_CLASS_CHOICES, null=False)
+    seat_number = models.CharField(max_length=3, null=False, help_text='A Letter and Two Digits (e.g. A09, B22, Z90)')
+    ticket_cost = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=True)
+
+    def save(self, *args, **kwargs):
+
+        if self.scheduled_flight_id and self.seat_class:
+            seat_class_dict = {"1st Class": 5000, "Business Class": 2500, "Premium Economy Class": 1000, "Regular Economy": 0}
+            seat_class_charge = seat_class_dict[self.seat_class]
+            self.ticket_cost = self.scheduled_flight_id.flight_cost + seat_class_charge
+
+        super(Ticket, self).save(*args, **kwargs)
+
+    def clean(self):
+        if not re.match(r'^[A-Z]\d{2}$', self.seat_number):
+            raise ValidationError('Seat Number Invalid; Must be a Letter + Two Digits')
+
+    def __str__(self):
+        return f"Ticket {self.ticket_id}: {self.booking_id} | {self.scheduled_flight_id}"
+
+
