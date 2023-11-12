2ef8426f57fb8770876e4c9f31cc226839243ef4
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2023-11-12 16:12:53 +0800

    Added Item and Passenger Models to app_booking

--- MagisAir/app_booking/admin.py
+++ MagisAir/app_booking/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Passenger, Item
 
 # Register your models here.
+@admin.register(Passenger)
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ('passenger_id', 'last_name', 'first_name', 'middle_initial', 'birthday', 'gender')
+    search_fields = ('last_name', 'first_name')
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('item_id', 'item_name', 'description', 'item_cost')
+    search_fields = ('item_name', 'description')
--- MagisAir/app_booking/models.py
+++ MagisAir/app_booking/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Passenger(models.Model):
+    passenger_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=255, null=False)
+    first_name = models.CharField(max_length=255, null=False)
+    middle_initial = models.CharField(max_length=1, null=True, blank=True)
+    birthday = models.DateField(null=False)
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Item(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=255, null=False)
+    description = models.CharField(max_length=255, null=False)
+    item_cost = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+
+    def __str__(self):
+        return f"{self.item_id}: {self.item_name}"
