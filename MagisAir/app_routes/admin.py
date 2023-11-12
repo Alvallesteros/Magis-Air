5e0004e79188e86d23076052fc302c8b82056eac
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2023-11-12 14:31:56 +0800

    Added Routes Admin Panel

--- MagisAir/app_routes/admin.py
+++ MagisAir/app_routes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Route, BaseFlight
 
 # Register your models here.
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ['route_id', 'origin', 'destination']
+    search_fields = ['origin', 'destination']
+
+@admin.register(BaseFlight)
+class BaseFlightAdmin(admin.ModelAdmin):
+    list_display = ['flight_code', 'flight_type', 'route']
+    search_fields = ['flight_code', 'flight_type']
