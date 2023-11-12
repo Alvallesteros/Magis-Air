52c0b57f2bb944f5639b7a299d7572d15a5e5fd5
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2023-11-12 13:48:42 +0800

    App URLs Setup

--- MagisAir/MagisAir/settings.py
+++ MagisAir/MagisAir/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_booking',
+    'app_crew',
+    'app_routes',
+    'app_schedule'
 ]
 
 MIDDLEWARE = [
--- MagisAir/MagisAir/urls.py
+++ MagisAir/MagisAir/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('routes/', include('app_routes.urls')),
+    path('', include('app_schedule.urls')),
+    path('crew/', include('app_crew.urls')),
+    path('booking/', include('app_booking.urls'))
 ]
