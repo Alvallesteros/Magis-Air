9b658547a98e1ebb16fd3fbd3cd5588a9e2ce531
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2023-11-12 14:43:25 +0800

    Created Crew models and admin panel

--- MagisAir/app_crew/models.py
+++ MagisAir/app_crew/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Crew Member
+class CrewMember(models.Model):
+    crew_member_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    middle_initial = models.CharField(max_length=5, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
