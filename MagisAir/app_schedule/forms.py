ee24a8acb42924b8a1ee4314a8de9f559fb40bff
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2023-11-13 23:28:11 +0800

    Added DateFilter Funcitonality to ScheduledFlights

---
+++ MagisAir/app_schedule/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class DateFilterForm(forms.Form):
+    filter_date = forms.DateField(label='Filter by Date', widget=forms.DateInput(attrs={'type': 'date'}))
--- MagisAir/app_schedule/views.py
+++ MagisAir/app_schedule/views.py
@@ -3,19 +3,29 @@ from django.shortcuts import render
 from django.db import connection
 from django.views import View
 from .models import ScheduledFlight
+from .forms import DateFilterForm
 
 # Create your views here.
 class ScheduledFlights(View):
     template_name = 'app_schedule/schedule.html'
 
     def get(self, request, *args, **kwargs):
-        with connection.cursor() as cursor:                 #SOURCE: https://docs.djangoproject.com/en/4.2/topics/db/sql/
-            cursor.execute( '''
-                    SELECT bf.flight_code, r.origin, r.destination, sf.departure_time AS "departure", sf.arrival_time AS "arrival", sf.duration
+        form = DateFilterForm(request.GET)
+        raw_query='''
+            SELECT bf.flight_code, r.origin, r.destination, sf.departure_time AS "departure", sf.arrival_time AS "arrival", sf.duration
                     FROM app_schedule_ScheduledFlight AS sf
                     JOIN app_routes_BaseFlight AS bf ON sf.base_flight_id=bf.id
                     JOIN app_routes_Route AS r ON bf.route_id=r.route_id
-                ''' )
+                    {_where}
+                    '''
+
+        with connection.cursor() as cursor:                             #SOURCE: https://docs.djangoproject.com/en/4.2/topics/db/sql/
+            if form.is_valid():
+                filter_date = form.cleaned_data['filter_date']                 
+                cursor.execute(raw_query.format(_where='WHERE sf.departure_date = %s'), [filter_date])
+            else:
+                cursor.execute(raw_query.format(_where=''))
+
             columns = [col[0] for col in cursor.description]
             results = [dict(zip(columns, row)) for row in cursor.fetchall()]
 
@@ -25,8 +35,11 @@ class ScheduledFlights(View):
                 r['duration'] = self.format_duration(r['duration'])
 
         context = { 'flights': results,
-                    'column_names': formatted_columns 
+                    'column_names': formatted_columns,
+                    'form': form,
+                    'date': filter_date
                     }
+
         return render(request, self.template_name, context)
 
     def format_duration(self, duration):
