51a1226c3b5a950d25e139542925ce1344ccfdee
Javi Ng <johnaidanvincent@gmail.com>
2023-11-14 18:57:41 +0800

    fixed query

--- MagisAir/app_crew/views.py
+++ MagisAir/app_crew/views.py
@@ -9,19 +9,29 @@ class CrewAssignments(View):
     template_name = 'app_crew/crew.html'
 
     def get(self, request, *args, **kwargs):
+
+        def dateTimeTransform(self, date, time):
+            return ""
+        
+        def nameTransform(self, first, last):
+            return ""
+
         query = '''
                 SELECT 
-                    CONCAT(cm.last_name, ', ', cm.first_name) AS "crew",
+                    cm.last_name,
+                    cm.first_name AS "crew",
                     ca.role,
                     bf.flight_code AS "flight",
                     fr.destination AS "destination",
-                    CONCAT(sf.departure_date, ', ', sf.departure_time) AS "departure",
-                    CONCAT(sf.arrival_date, ', ', sf.arrival_time) AS "arrival"
+                    sf.departure_date AS "departure",
+                    sf.departure_time,
+                    sf.arrival_date AS "arrival",
+                    sf.arrival_time
                     FROM app_crew_CrewMember AS cm
-                    JOIN app_crew_CrewAssignment AS ca ON cm.crew_member_id=ca.crew_member
-                    JOIN app_schedule_ScheduledFlight AS sf ON ca.scheduled_flight=sf.scheduled_flight_id
-                    JOIN app_routes_BaseFlight AS bf ON sf.base_flight=bf.id
-                    JOIN app_routes_Route AS fr ON bf.route=fr.route_id
+                    JOIN app_crew_CrewAssignment AS ca ON cm.crew_member_id = ca.crew_member_id
+                    JOIN app_schedule_ScheduledFlight AS sf ON ca.scheduled_flight_id = sf.scheduled_flight_id
+                    JOIN app_routes_BaseFlight AS bf ON sf.base_flight_id=bf.id
+                    JOIN app_routes_Route AS fr ON bf.route_id=fr.route_id
                     {_where}
                 '''
         
