c7802f4ec1bdbdb3a081ca68a3b0ff0ba1f05e3a
Javi Ng <johnaidanvincent@gmail.com>
2023-11-14 23:36:31 +0800

    fixed datetime, name formatting

--- MagisAir/app_crew/views.py
+++ MagisAir/app_crew/views.py
@@ -3,36 +3,37 @@ from django.shortcuts import render
 from django.db import connection 
 from django.views import View 
 from .models import CrewMember, CrewAssignment
+import datetime as dt
 
 # Create your views here.
 class CrewAssignments(View):
     template_name = 'app_crew/crew.html'
 
-    def get(self, request, *args, **kwargs):
-
-        def dateTimeTransform(self, date, time):
-            return ""
+    def dateTimeTransform(self, date, time):
+            return date.strftime('%-d %B %Y') + ', ' + time.strftime('%H:%M')
         
-        def nameTransform(self, first, last):
-            return ""
+    def nameTransform(self, first, last):
+        return last + ', ' + first
 
+    def get(self, request, *args, **kwargs):
         query = '''
                 SELECT 
                     cm.last_name,
+                    sf.arrival_time,
+                    sf.departure_time,
                     cm.first_name AS "crew",
                     ca.role,
                     bf.flight_code AS "flight",
                     fr.destination AS "destination",
                     sf.departure_date AS "departure",
-                    sf.departure_time,
-                    sf.arrival_date AS "arrival",
-                    sf.arrival_time
+                    sf.arrival_date AS "arrival"
                     FROM app_crew_CrewMember AS cm
                     JOIN app_crew_CrewAssignment AS ca ON cm.crew_member_id = ca.crew_member_id
                     JOIN app_schedule_ScheduledFlight AS sf ON ca.scheduled_flight_id = sf.scheduled_flight_id
                     JOIN app_routes_BaseFlight AS bf ON sf.base_flight_id=bf.id
                     JOIN app_routes_Route AS fr ON bf.route_id=fr.route_id
                     {_where}
+                    ORDER BY bf.flight_code
                 '''
         
         with connection.cursor() as cursor:
@@ -40,7 +41,12 @@ class CrewAssignments(View):
             columns = [col[0] for col in cursor.description]
             results = [dict (zip(columns, row)) for row in cursor.fetchall()]
 
-            formatted_columns = [col.title() for col in columns]
+            formatted_columns = [col.title() for col in columns][3:]
+
+            for r in results:
+                r['departure'] = self.dateTimeTransform(r['departure'], r['departure_time'])
+                r['arrival'] = self.dateTimeTransform(r['arrival'], r['arrival_time'])
+                r['crew'] = self.nameTransform(r['crew'], r['last_name'])
 
         context = {
             'headers' : formatted_columns,
