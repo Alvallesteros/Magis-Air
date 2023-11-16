3b737a5b5d8d3e5005c37b9f3882c3685e386d23
Anthony <anthony.bicomong@obf.ateneo.edu>
2023-11-16 23:15:04 +0800

    routes views and template hihi

--- MagisAir/app_routes/views.py
+++ MagisAir/app_routes/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import BaseFlight
+from django.views import View 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Routes App")
+
+def flight_list(request):
+    flights = BaseFlight.objects.all()
+    return render(request, 'app_routes/routes.html', {'flights': flights})
