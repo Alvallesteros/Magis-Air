ae1d39c1733ea62c45e8b25dfc56fb9d56ef4e88
justin <justinreyes13003@gmail.com>
2023-11-26 23:03:59 +0800

    flskdjflksjf

--- MagisAir/app_schedule/forms.py
+++ MagisAir/app_schedule/forms.py
@@ -3,14 +3,7 @@ from django import forms
 
 class DateFilterForm(forms.Form):
     filter_date = forms.DateField(
-<<<<<<< HEAD
         label='Filter by Date',
         required=False,
         widget=forms.DateInput(attrs={'type': 'date', 'required': 'True'}),
     )
-=======
-        label='Filter by Date', 
-        widget=forms.DateInput(attrs={'type': 'date'}),
-        required=False
-        )
->>>>>>> views-schedule-frontend
